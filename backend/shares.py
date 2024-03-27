647c6e299a23d0629b1ed9b58dc26f576ef4a807
Arturo Gonzalez <arturogonzalez@192-168-1-107.tpgi.com.au>
2024-03-27 16:16:46 +1100

    Improved shares.py

--- backend/shares.py
+++ backend/shares.py
@@ -1,5 +1,8 @@
+# At the beginning of your script
 """Module docstring: This module contains the Streamlit app for displaying different financial data types."""
+# This is the module-level docstring describing what this module does.
 
+# Imports come here
 import tracemalloc
 import warnings
 from io import StringIO
@@ -14,7 +17,13 @@ tracemalloc.start()
 warnings.filterwarnings("ignore", message="coroutine 'expire_cache' was never awaited", category=RuntimeWarning)
 
 
+# Function to plot the candlestick chart
 def plot_candlestick_chart(df):
+    """
+    Plot candlestick chart based on the provided DataFrame.
+    :param df: DataFrame containing the data to plot.
+    :return: None
+    """
     if 'timestamp' in df.columns:
         fig = go.Figure(data=[go.Candlestick(x=df['timestamp'],
                                              open=df['open'],
@@ -28,14 +37,19 @@ def plot_candlestick_chart(df):
         st.warning("DataFrame does not contain 'timestamp' column.")
 
 
+# Function to set up page configurations
 def setup_page_config():
-    """Sets up page configurations."""
+    """
+    Sets up page configurations.
+    :return: None
+    """
     st.set_page_config(page_title="Streamlit Financial Data Visualization App",
                        page_icon=":chart_with_upwards_trend:",
                        layout='wide')
     st.image("images/algoAI_large.png", width=300)  # Adjust width as needed
 
 
+# Function to create tabs for different types of financial data
 def display_tabs():
     """
     Creates tabs for different types of financial data.
@@ -50,8 +64,13 @@ def display_tabs():
                     "Technical Indicators"])
 
 
+# Functionality for the Share Prices tab
 def share_prices(tab):
-    """Functionality for the Share Prices tab."""
+    """
+    Functionality for the Share Prices tab.
+    :param tab: The current tab.
+    :type tab: streamlit.Tabs
+    """
     with tab:
         # Configuration inputs in the sidebar
         with st.sidebar:
@@ -70,7 +89,6 @@ def share_prices(tab):
                 'output_size': st.selectbox('Output Size', ['compact', 'full'], key="share_output_size"),
                 'extended': st.toggle('Extended Hours', False, key="share_extended"),
                 'month': st.text_input('Month', '2009-01', key="share_month"),
-                # 'month': st.date_input('Month', value=pd.to_datetime('2009-01'), key="share_month"),
                 'api_key': st.text_input('API Key', 'demo', key="share_api_key", type="password")
             }
 
@@ -92,21 +110,26 @@ def share_prices(tab):
             data_io = StringIO(data)
             df = pd.read_csv(data_io)
 
-            # st.subheader('Candlestick Chart')
+            # Plot candlestick chart
             plot_candlestick_chart(df)
 
-            # st.subheader('Share Prices Table')
-            # Set the height of the table and make it scrollable
+            # Display DataFrame with adjustable width
             st.dataframe(df.style.set_table_styles(
                 [dict(selector='table', props=[('max-height', '400px'), ('overflow-y', 'scroll'), ('width', '100%')])]
             ), width=1300)
 
 
+# Class for handling API requests
 class APIRequest:
     @staticmethod
     @st.cache_data(ttl=600)  # Cache data for 10 minutes; adjust TTL as needed
     def get_data(url):
-        """Fetch data from the constructed URL."""
+        """
+        Fetch data from the constructed URL.
+        :param url: The URL to fetch data from.
+        :return: The data fetched from the URL.
+        :rtype: str
+        """
         response = requests.get(url, timeout=10)  # Timeout after 10 seconds
         return response.text  # Make sure to return response.text, not just response
 
@@ -119,6 +142,7 @@ class APIRequest:
         raise NotImplementedError("Subclass must implement abstract method")
 
 
+# Class for handling requests to the Alpha Vantage API
 class AlphaVantageRequest:
     """Handles construction and representation of requests to the Alpha Vantage API."""
 
@@ -127,6 +151,12 @@ class AlphaVantageRequest:
         self.config = config
 
     def build_url(self):
+        """
+        Constructs the URL for the API request.
+        :return: The constructed URL.
+        :rtype: str
+        :raises NotImplementedError: If the function is not implemented by the subclass.
+        """
         extended_str = 'true' if self.config['extended'] else 'false'
         return (f"https://www.alphavantage.co/query"
                 f"?function={self.config['function']}"
